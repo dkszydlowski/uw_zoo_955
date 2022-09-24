239902e1e6edd2a1b66fed6c3b11331adbf3b1f0
Daniel Szydlowski <dszydlowski@wisc.edu>
2022-09-24 11:39:03 -0500

    homework

--- tidyverse_homework.R
+++ tidyverse_homework.R
@@ -49,16 +49,24 @@ iris_means <- summarize(group_by(iris, Species), mn.petal.length = mean(Petal.Le
 # 5. pipes
 # the above can get unwieldy - rearrange iris_means from 4 using pipes
 
+# just arrange so it is sorted by mean, weren't sure what else needs to be rearranged
+iris_means = iris_means %>% arrange(by = mn.petal.length)
 
 
 ## On Your Own #1 
 # now compute mean petal area for each species - how would you go about it using dplyr
 
-
-iris_mean_area <- iris %>% 
-  summarize(group_by(Species), mn.petal.area = mean(Petal.area))
+iris_mean_area <- iris %>% group_by(Species) %>% 
+  summarise(mn.petal.area = mean(Petal.Length * Petal.Width)) %>% 
+  ungroup()
 
 # Q: What is the mean petal area for each species
+
+#-------------------------------Question 1 answer ----------------------------------
+# Setosa = 0.366
+# versicolor = 5.72
+# virginica = 11.3
+#------------------------------------------------------------------------------------
 
 # 6. arrange/select/count
 # determine which species has the longest mean petal length
@@ -69,16 +77,85 @@ iris_size <-
   summarize(mn.petal.length = mean(Petal.Length)) %>%
   arrange(desc(mn.petal.length))
 
+# virginica has the longest mean petal length of 5.552
+
 # On Your Own #2
-# do the same for the other measurements (i.e. petal.width, sepal.length, etc)
+# do the same for the other measurements (i.e. petal.width, sepal.length, sepal.width, etc)
+iris_size <- 
+  iris %>% 
+  select(Species, Petal.Width) %>% # only selects these 2 columns
+  group_by(Species) %>%
+  summarize(mn.petal.width = mean(Petal.Width)) %>%
+  arrange(desc(mn.petal.width))
+
+# virginica has the longest mean petal width of 2.026
+
+iris_size <- 
+  iris %>% 
+  select(Species, Sepal.Length) %>% # only selects these 2 columns
+  group_by(Species) %>%
+  summarize(mn.sepal.length = mean(Sepal.Length)) %>%
+  arrange(desc(mn.sepal.length))
+
+# virginica also has the longest mean sepal length of 6.588
+
+iris_size <- 
+  iris %>% 
+  select(Species, Sepal.Width) %>% # only selects these 2 columns
+  group_by(Species) %>%
+  summarize(mn.sepal.width = mean(Sepal.Width)) %>%
+  arrange(desc(mn.sepal.width))
+
+# setosa has the longest mean sepal width of 3.428
+
 # Q: What is the mean petal and sepal lengths and widths for each species
+
+# ------------------------- Question 2 answer ---------------------------------
+# petal length:
+# virginica             5.55
+# versicolor            4.26
+# setosa                1.46
+
+# petal width:
+# virginica           2.03 
+# versicolor          1.33 
+# setosa              0.246
+
+# sepal length: 
+# virginica             6.59
+# versicolor            5.94
+# setosa                5.01
+
+# sepal width:
+# setosa               3.43
+# virginica            2.97
+# versicolor           2.77
+
+#-------------------------------------------------------------------------------
 
 # count the number of records for each species
 (iris_spp_n <- count(iris, Species))
 
 # On Your Own #3
 # count the number of samples that are >= mean.petal.length for each species
+iris_spp_n <- iris %>% 
+  group_by(Species) %>% 
+  mutate(mean = mean(Petal.Length)) %>% 
+  mutate(exceed_mean = Petal.Length > mean)
+
+iris_spp_exceed_mean = iris_spp_n %>% group_by(Species) %>% 
+  summarize(sum(exceed_mean))
+
+
 # Q: How many samples where Petal.Length >= mean.petal.length does each species have
+
+#------------------------ Question 3 answer ------------------------------------
+
+# 1 setosa                     26
+# 2 versicolor                 27
+# 3 virginica                  25
+
+#-------------------------------------------------------------------------------
 
 # 7. joins
 set.seed(123)
@@ -88,6 +165,15 @@ wt <- data.frame(level = LETTERS[1:6], weight = sample(50:500, 6, replace = TRUE
 # join together height and weight by level
 # what happens when you reverse ht and wt (i.e. put wt first, then ht)
 ht_wt <- left_join(ht, wt, by = "level")
+
+ht_wt <- left_join(wt, ht, by = "level")
+
+# when ht and wt are reversed, the columns are swapped and there is an extra observation
+# for wt which was previously dropped because height was missing
+
+
+
+
 
 # On Your Own #4 - Extra Credit
 # work with the nycflights13 data set to determine what airport had the 
